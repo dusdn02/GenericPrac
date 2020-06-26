150533c650ca9b5eed29c22b62f045b5441abfef
dusdn02 <lyw9114@naver.com>
2020-06-26 12:06:16 +0900

    초기 커밋.

---
+++ GenericPrac/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPrac
+{
+    class Wanted<T>
+    {
+        public T Value;
+        public Wanted(T value)
+        {
+            this.Value = value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wanted<string> wantedString = new GenericPrac.Wanted<string>("루피");
+            Wanted<int> wantedInt = new GenericPrac.Wanted<int>(100);
+            Wanted<double> wantedDouble = new GenericPrac.Wanted<double>(3.141592);
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedInt.Value);
+            Console.WriteLine(wantedDouble.Value);
+        }
+    }
+}
